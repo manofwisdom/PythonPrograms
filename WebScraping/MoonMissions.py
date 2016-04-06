37ce57d90853748ff2331ccfb8340d42a34822ba
Alok Sharma <aloks1701@gmail.com>
2016-04-06 23:16:59 +0530

    Update MoonMissions.py

--- WebScraping/MoonMissions.py
+++ WebScraping/MoonMissions.py
@@ -1,1 +1,32 @@
 
+import requests
+from BeautifulSoup import BeautifulSoup as BS
+
+url = "https://en.wikipedia.org/wiki/List_of_missions_to_the_Moon"
+r = requests.get(url)
+soup =BS(r.content)
+
+tables = soup.findAll('table')[0].findAll('tr')
+print len(tables)
+#print tables
+
+spacecraft =[]
+launch_date = []
+mission = []
+outcome = []
+
+p = ""
+q = ""
+r = ""
+s = ""
+
+for row in tables[1:126]:
+    p = row.findAll('td')[0].text
+    q = row.findAll('td')[1].text
+    r = row.findAll('td')[4].text
+    s = row.findAll('td')[5].text
+    #print p,"\t\t", r,"\t\t",s
+    spacecraft.append(p)
+    launch_date.append(q)
+    mission.append(r)
+    outcome.append(s)
